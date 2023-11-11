c41a5830721246c685c9817070aecb3beab9b863
Humberto Rocha <humrochagf@gmail.com>
2023-11-11 14:49:31 -0500

    Add authentication, using result from another request, and payload from file

--- src/api_explorer/__about__.py
+++ src/api_explorer/__about__.py
@@ -1,4 +1,1 @@
-# SPDX-FileCopyrightText: 2023-present Humberto Rocha <humrochagf@gmail.com>
-#
-# SPDX-License-Identifier: MIT
 __version__ = "0.0.1"
--- src/api_explorer/core.py
+++ src/api_explorer/core.py
@@ -1,4 +1,6 @@
+import json
 import logging
+from pathlib import Path
 
 from httpx import AsyncClient
 from rich import print
@@ -11,13 +13,41 @@ logging.basicConfig(
     level="DEBUG", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
 )
 
+DATA_FOLDER = Path(__file__).parent / "data"
+
 
 async def search_images(client: AsyncClient) -> dict:
-    response = await client.get(f"{settings.base_url}/images/search")
+    response = await client.get(
+        f"{settings.base_url}/images/search",
+        params={"has_breeds": True},
+    )
 
     return response.json()
 
 
+async def get_breed(client: AsyncClient, breed_id: str) -> dict:
+    response = await client.get(f"{settings.base_url}/breeds/{breed_id}")
+
+    return response.json()
+
+
+async def vote_from_file(client: AsyncClient) -> dict:
+    with (Path(DATA_FOLDER) / "vote.json").open() as fp:
+        data = json.load(fp)
+        response = await client.post(f"{settings.base_url}/votes", json=data)
+
+        return response.json()
+
+
 async def main() -> None:
-    async with AsyncClient() as client:
-        print(await search_images(client))
+    headers = {
+        "x-api-key": settings.auth_token,
+    }
+
+    async with AsyncClient(headers=headers) as client:
+        cat = await search_images(client)
+        print(cat)
+
+        print(await get_breed(client, cat[0]["breeds"][0]["id"]))
+
+        print(await vote_from_file(client))
